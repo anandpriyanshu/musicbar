40a48dde39c094088ed475bd7935648ecb059115
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-08-27 09:45:12 +0530

    Create Navbar using Aceternity ui. Enable dark mode in twindconfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">HI there</h1>
+    <>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02">
+        <h1 className="text-2xl text-center">HI there</h1>
+      </main>
+    </>
   );
 }
