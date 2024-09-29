77ee8d3555f91808dd600a686891ea2a5c46fec2
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-09-29 09:28:41 +0530

    Remove unnecesssary lines

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -8,28 +8,28 @@ const Instructors = () => {
     const instructors = [
         {
             id: 1,
-            name: 'Elena Briggs',
+            name: 'Elen Davis',
             designation: 'Vocal Coach',
             image:
                 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
         },
         {
             id: 2,
-            name: 'Marcus Reid',
+            name: 'Andrew Thomas',
             designation: 'Guitar Instructor',
             image:
                 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80',
         },
         {
             id: 3,
-            name: 'Julia Zhang',
+            name: 'Juli Sweet',
             designation: 'Piano Teacher',
             image:
                 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
         },
         {
             id: 4,
-            name: 'Andre Gomez',
+            name: 'Nick Gomez',
             designation: 'Drumming Expert',
             image:
                 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
@@ -40,7 +40,7 @@ const Instructors = () => {
             <div className='relative h-[40rem] flex justify-center items-center overflow-hidden '>
 
                 <WavyBackground className='w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full'>
-                    <h2 className='mt-20 md:mt-0 text-3xl md:text-4xl lg:text-7xl text-white font-bold mb-8 '>Meet Our Instructors</h2>
+                    <h2 className='mt-20 md:mt-0 text-3xl md:text-4xl lg:text-7xl text-white font-bold mb-8 '> Our True Instructors</h2>
                     <p className='text-base md:text-lg text-white text-center mb-4 '>Discover the talented professionals who will guide your musical journey</p>
 
                     <div className='flex flex-row justify-center items-center mb-10 w-full'>
